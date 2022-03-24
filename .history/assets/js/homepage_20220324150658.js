e99ac172339db364f77e04f2fe1ad88f470555a2
cobalt88 <vincentteune@gmail.com>
2022-03-24 17:46:38 -0400

    3.24.22
    moving on to the next issue

---
+++ .history/assets/js/homepage_20220324150658.js
@@ -0,0 +1,43 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      displayRepos(data, user);
+    });
+  });
+};
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  // get value from input element
+var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+  // console.log(event);
+};
+
+var displayRepos = function(repos, searchTerm) {
+  console.log(repos);
+  console.log(searchTerm);
+  repoContainerEl.textContent = "";
+  repoSearchTerm.textContent = searchTerm;
+};
+
+
+
+getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
